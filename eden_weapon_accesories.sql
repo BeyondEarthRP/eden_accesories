b517dcd20a6edca3524cf9a75d2af6dc23d60aa6
BeyondEarthRP <BeyondRageRP@gmail.com>
2019-12-22 10:05:49 +0000

    converted to english

--- eden_weapon_accesories.sql
+++ eden_weapon_accesories.sql
@@ -1,4 +1,6 @@
-INSERT INTO `items` (`id`, `name`, `label`, `limit`, `rare`, `can_remove`) VALUES (2010, 'yusuf', 'Skin de luxe', -1, 0, 1);
-INSERT INTO `items` (`id`, `name`, `label`, `limit`, `rare`, `can_remove`) VALUES (209, 'grip', 'Poignée', -1, 0, 1);
-INSERT INTO `items` (`id`, `name`, `label`, `limit`, `rare`, `can_remove`) VALUES (208, 'flashlight', 'Lampe', -1, 0, 1);
-INSERT INTO `items` (`id`, `name`, `label`, `limit`, `rare`, `can_remove`) VALUES (207, 'silencieux', 'Silencieux', -1, 0, 1);
+USE `essentialmode`;
+
+INSERT INTO `items` (`id`, `name`, `label`, `limit`, `rare`, `can_remove`) VALUES (2010, 'yusuf', 'Luxury Skin', -1, 0, 1);
+INSERT INTO `items` (`id`, `name`, `label`, `limit`, `rare`, `can_remove`) VALUES (209, 'grip', 'Grip', -1, 0, 1);
+INSERT INTO `items` (`id`, `name`, `label`, `limit`, `rare`, `can_remove`) VALUES (208, 'flashlight', 'Flashlight', -1, 0, 1);
+INSERT INTO `items` (`id`, `name`, `label`, `limit`, `rare`, `can_remove`) VALUES (207, 'silencieux', 'Suppressor', -1, 0, 1);
